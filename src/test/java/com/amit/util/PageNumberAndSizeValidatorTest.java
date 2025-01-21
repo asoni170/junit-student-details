dca51a687215f9b2aa1ee39245019e241dba9c0a
Amit Soni <asoni170@gmail.com>
2025-01-21 17:46:05 +0530

    unused import removed

--- src/test/java/com/amit/util/PageNumberAndSizeValidatorTest.java
+++ src/test/java/com/amit/util/PageNumberAndSizeValidatorTest.java
@@ -3,8 +3,6 @@ package com.amit.util;
 import com.amit.exception.PageInvalidException;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.runner.RunWith;
@@ -15,7 +13,6 @@ import static com.amit.util.constant.CommonConstants.INVALID_PAGE_NUMBER;
 import static com.amit.util.constant.CommonConstants.INVALID_PAGE_SIZE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mockStatic;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
